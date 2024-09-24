5d523405f2ced32c0921a279f7636fba96878e17
Aaron Wolf <aaron.wolf@gmail.com>
2024-09-24 20:38:18 +0300

    todo list

--- 08-JS/day5/todo/script.js
+++ 08-JS/day5/todo/script.js
@@ -1,17 +1,54 @@
 const tasks = [];
 
 function listTasks() {
-  todoList = document.getElementById('todoList');
+  const todoList = document.getElementById('todoList');
   todoList.innerHTML = '';
+
   tasks.forEach((task, index) => {
     const todo = document.createElement('li');
     todo.className = 'list-group-item';
-    todo.innerHTML = `
-            <div id="task-${index}" class="lineItem-container">
-              <input class="form-check-input check" type="checkbox" >
-              <div class="lineItem">${task} <i class="bi bi-trash"></i></div>
-            </div>
-    `;
+
+    const lineItemContainer = document.createElement('div');
+    lineItemContainer.className = 'lineItem-container';
+    lineItemContainer.id = `task-${index}`;
+
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
+    checkBox.className = 'form-check-input check';
+
+    const lineItem = document.createElement('div');
+    lineItem.className = 'lineItem';
+    lineItem.textContent = task.description;
+
+    if (task.completed) {
+      checkBox.checked = true;
+      lineItem.style.textDecoration = 'line-through';
+    } else {
+      checkBox.checked = false;
+    }
+
+    checkBox.addEventListener('change', (e) => {
+      if (e.target.checked) {
+        lineItem.style.textDecoration = 'line-through';
+        task['completed'] = true;
+      } else {
+        lineItem.style.textDecoration = 'none';
+        task['completed'] = false;
+      }
+    });
+
+    const trashIcon = document.createElement('i');
+    trashIcon.className = 'bi bi-trash trash';
+
+    trashIcon.addEventListener('click', (e) => {
+      e.preventDefault();
+      removeTask(index);
+    });
+
+    lineItemContainer.appendChild(checkBox);
+    lineItemContainer.appendChild(lineItem);
+    lineItem.appendChild(trashIcon);
+    todo.appendChild(lineItemContainer);
     todoList.appendChild(todo);
   });
 }
@@ -26,10 +63,17 @@ function addTask() {
     if (inputField.value.trim() == '') {
       return;
     }
-    tasks.push(inputField.value);
+    tasks.push({ description: inputField.value, isDone: false });
     listTasks();
     todoItem.reset();
   });
 }
 
 addTask();
+
+function removeTask(index) {
+  tasks.splice(index, 1);
+  listTasks();
+}
+
+removeTask();
